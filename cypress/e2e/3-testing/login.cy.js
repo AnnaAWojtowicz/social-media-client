81f95d803a50650a92693813629895da35028895
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2023-11-11 12:27:36 +0100

    .env and updates gitignore & package json

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -1,3 +1,10 @@
 it('logs in the user', () => {
+  // cy.intercept('https://nf-api.onrender.com/api/v1"').as('exampleModal');
   cy.visit(' http://127.0.0.1:8080');
+  //  cy.wait("@exampleModal");
+  cy.wait(5000);
+  //cy.get("#registerForm").should("be.visible");
+  cy.get('#registerForm > .modal-footer > .btn-outline-success').click();
+  // cy.get("#loginEmail").type("");
+  // cy.get("#loginPassword").type("");
 });
