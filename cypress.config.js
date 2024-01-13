9d78ddba7715cd9232684e34162013324bc820c0
Anna Wojtowicz <annwoj26185@stud.noroff.no>
2024-01-13 22:01:51 +0100

    resit changes implemented

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     env: {
-      baseUrl: 'https://annaawojtowicz.github.io/social-media-client/',
+      baseUrl: '/',
       loginUser: 'test1234@stud.noroff.no',
       loginPassword: 'test1234',
     },
